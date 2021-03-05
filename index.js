a5de1d1daa97cd5ce20c0978a2a9e49e9082f337
LokeshShelva <58987457+LokeshShelva@users.noreply.github.com>
2021-03-05 20:59:03 +0530

    new

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+var bodyParser = require("body-parser");
+var fs = require("fs")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
-app.get('/downloads', function (req, res) {
+var raw = fs.readFileSync(__dirname + "/list.json")
+var jsonraw = JSON.parse(raw)
+var list = jsonraw["data"]
+
+app.post('/downloads', function (req, res) {
+
     // res.download(__dirname + `/pdf/page${id}.pdf`);
-    console.log(req.body)
+    var files = path.join(__dirname, "/pdf")
+    var name = req.body.Name.toString()
+    var Fname = req.body["Father name"].toString()
+    var id = -1
+
+    for (var i = 0; i < list.length; i++) {
+        if (list[i]["Name"] == name && list[i]["Father Name"] == Fname) {
+            id = list[i]["No"]
+        }
+    }
+    if (id != -1) {
+        res.status(200).send(JSON.stringify({ "id": id }))
+    } else {
+        res.status(200).send("No file")
+    }
 });
 
 app.listen(port, () => console.log("listening.."))
