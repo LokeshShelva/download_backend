3464de87b2b9f1cb20e26091630e09e327db21f5
LokeshShelva <58987457+LokeshShelva@users.noreply.github.com>
2021-03-05 19:16:40 +0530

    changed port

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
+const port = process.env.PORT || 3000
+
 app.get('/downloads', function (req, res) {
     // res.download(__dirname + `/pdf/page${id}.pdf`);
     console.log(req.body)
 });
 
-app.listen(3000, () => console.log("listening.."))
+app.listen(port, () => console.log("listening.."))
